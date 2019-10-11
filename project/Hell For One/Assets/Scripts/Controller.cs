b271c9ead97fa7885c9867c295275edc34d754fc
andreacarrarini <andrea.carrarini.18@gmail.com>
2019-10-11 18:34:40 +0200

    character controller & camera started

---
+++ project/Hell For One/Assets/Scripts/Controller.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    public Rigidbody rb;
+    public float zMovement, xMovement, resultingMovement;
+    public Vector3 newPosition;
+    public Camera camera;
+    public GameObject mainCamera;
+
+    public float yRotation;
+    public float xRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCamera = GameObject.FindGameObjectWithTag( "MainCamera" );
+        camera = mainCamera.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+
+        // Horizontal2 non funge bene
+        yRotation = Input.GetAxis( "Horizontal2" );
+        xRotation = Input.GetAxis( "Vertical2" );
+
+        zMovement = Input.GetAxis( "Vertical" );
+        xMovement = Input.GetAxis( "Horizontal" );
+
+        if ( zMovement != 0f || xMovement != 0f )
+        {
+            // Make it move 10 meters per second instead of 10 meters per frame...
+            zMovement *= Time.deltaTime;
+            xMovement *= Time.deltaTime;
+
+            Vector3 newZPosition = new Vector3( 0, 0, zMovement );
+            Vector3 newXPosition = new Vector3( xMovement, 0, 0 );
+
+            Vector3 resultingNewPosition = newZPosition + newXPosition;
+            resultingNewPosition *= 10;
+
+            //Quaternion orientation = new Quaternion( 0, xMovement, 0, 0 );
+            //transform.rotation = orientation;
+
+            //newPosition = new Vector3(  - transform.position.x, 0, zMovement - transform.position.z );
+
+            transform.rotation = Quaternion.LookRotation( resultingNewPosition );
+            transform.position += new Vector3( newXPosition.x, 0, newZPosition.z ).normalized * 10 * Time.deltaTime;
+
+            mainCamera.transform.position += new Vector3( newXPosition.x, 0, newZPosition.z ).normalized * 10 * Time.deltaTime;
+
+            // Move translation along the object's z-axis
+            //transform.Translate( 0, 0, zMovement );
+
+
+            // Rotate around our y-axis
+            //We don't want to rotate, we want a sharp turn and then he goes that way
+            //transform.Rotate( 0, xMovement, 0 );
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        //transform.SetPositionAndRotation( newPosition, Quaternion.identity );
+        //transform.Translate( 0, 0, resultingMovement );
+        //rb.AddForce(0, 0, translation);
+    }
+}
---
+++ project/Hell For One/Assets/Scripts/cameraManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraManager : MonoBehaviour
+{
+
+    GameObject player;
+    public float yRotation;
+    public Vector3 rotation;
+    public float cameraDistance = 20f;
+    public float cameraHeight = 7.5f;
+
+    Vector3 cameraHeightFixed;
+    Vector3 cameraToPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag( "Player" );
+    }
+
+    // TODO if I move then rotate camera ==> MESS
+    void Update()
+    {
+        yRotation = Input.GetAxis( "Vertical2" );
+        transform.LookAt( player.transform );
+
+        if ( yRotation != 0f )
+        {
+            rotation = Vector3.right * yRotation;
+
+            transform.Translate( rotation );
+
+            cameraToPlayer = player.transform.position - transform.position;
+            transform.position += (cameraToPlayer - cameraToPlayer.normalized * 20);
+
+            // Don't do new in Update()!!!
+            cameraHeightFixed = new Vector3( transform.position.x, cameraHeight, transform.position.z );
+
+            //Vector3 cameraHeightVector = new Vector3( transform.position.x, cameraHeight , transform.position.z );
+            transform.position += cameraHeightFixed - cameraHeightFixed.normalized * 7.5f;
+
+            cameraDistance = cameraToPlayer.magnitude;
+
+            //transform.Translate( Vector3.forward * Time.deltaTime * 30 );
+        }
+    }
+}
