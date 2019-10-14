49691976459becb15e10d0fe446eca1c49554dd9
andreacarrarini <andrea.carrarini.18@gmail.com>
2019-10-14 18:18:55 +0200

    camera & controller work

--- project/Hell For One/Assets/Scripts/Controller.cs
+++ project/Hell For One/Assets/Scripts/Controller.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    //TODO Organize attributes with [Header("Inputs")], [Header("Stats")] ...
 
     public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
+    //public float rotationSpeed = 100.0f;
     public Rigidbody rb;
     public float zMovement, xMovement, oldZMovement, oldXMovement, resultingMovement;
     public Vector3 newZPosition, newXPosition, resultingNewPosition, resultingNewPositionStillMoving;
@@ -15,15 +16,20 @@ public class Controller : MonoBehaviour
     public GameObject mainCamera;
     public cameraManager cameraManager;
     public Vector3 mainCameraForward;
+    public float moveAmount, moveDir;
+    public float rotateSpeed = 5f;
 
     public float yRotation;
     public float xRotation;
+
+    // Used to set the movement direction when the player stops
+    public Vector3 cameraForward;
 
     // Indicative one
     private float cameraDistanceFromPlayer = 40f;
 
     // Used to move in the same direction even if the camera rotates (if the movement is continuos)
-    public bool stillMoving = true;
+    //public bool stillMoving = false;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +41,7 @@ public class Controller : MonoBehaviour
         newZPosition = new Vector3();
         newXPosition = new Vector3();
         resultingNewPosition = new Vector3();
+        cameraForward = Camera.main.transform.forward;
     }
 
     // TODO add a way to keep moving in the same direction even if the camera rotates (only if left stick input is continuos),
@@ -74,23 +81,50 @@ public class Controller : MonoBehaviour
 
             //transform.position += resultingNewPositionStillMoving.normalized * 10 * Time.deltaTime;
 
-            //ORIGINAL      
-            resultingNewPosition = new Vector3();
-            resultingNewPosition.z = Camera.main.transform.forward.z * zMovement;
-            resultingNewPosition.y = 0f;
-            resultingNewPosition.x = Camera.main.transform.right.x * xMovement;
+            //ORIGINAL 
 
-            transform.rotation = Quaternion.LookRotation( resultingNewPosition );
+            //transform.rotation = Quaternion.LookRotation( resultingNewPosition );
 
-            var CharacterRotation = Camera.main.transform.rotation;
-            CharacterRotation.x = 0;
-            CharacterRotation.z = 0;
+            //Vector3 cameraOrientation = Camera.main.transform.rotation.eulerAngles;
+            //cameraOrientation.x = 0;
+            //cameraOrientation.z = 0;
+            //transform.rotation = Quaternion.Euler( cameraOrientation );
 
-            transform.rotation = CharacterRotation;
+            //var CharacterRotation = Camera.main.transform.rotation;
+            //CharacterRotation.x = 0;
+            //CharacterRotation.z = 0;
+
+            //transform.rotation = CharacterRotation;
 
             //transform.position += resultingNewPosition.normalized * (10 * Time.deltaTime);
 
-            transform.position += resultingNewPosition * (10 * Time.deltaTime);
+            Vector3 vertical = zMovement * Camera.main.transform.forward;
+            Vector3 horizontal = xMovement * Camera.main.transform.right;
+            Vector3 moveDir = (vertical + horizontal).normalized;
+            moveDir.y = 0f;
+            float m = Mathf.Abs( zMovement ) + Mathf.Abs( xMovement );
+            moveAmount = Mathf.Clamp01( m );
+            transform.position += moveDir.normalized * 10 * Time.deltaTime;
+
+            Vector3 targetDir = moveDir;
+            targetDir.y = 0f;
+            if ( targetDir == Vector3.zero )
+                targetDir = Vector3.forward;
+            Quaternion tr = Quaternion.LookRotation( targetDir );
+            Quaternion targetRotation = Quaternion.Slerp( transform.rotation, tr, moveAmount * rotateSpeed );
+            transform.rotation = targetRotation;
+
+
+
+            //resultingNewPosition = new Vector3();
+            //resultingNewPosition.z = mainCamera.transform.forward.z * zMovement;
+            //resultingNewPosition.y = 0f;
+            //resultingNewPosition.x = mainCamera.transform.right.x * xMovement;
+
+
+            //transform.position += resultingNewPosition * (10 * Time.deltaTime);
+
+            //transform.Translate( resultingNewPosition * 10 * Time.deltaTime, Space.Self );
 
             //TRY WITH PLAYER ORIENTATION (THINK I NEED TO USE Transform.InverseTransformDirection())
             //resultingNewPosition = new Vector3();
@@ -116,7 +150,7 @@ public class Controller : MonoBehaviour
             //oldXMovement = xMovement;
         }
 
-        else if ( (zMovement != 0f || xMovement != 0f) && (!stillMoving) )
+        else if ( (zMovement != 0f || xMovement != 0f) )
         {
 
             //// Make it move 10 meters per second instead of 10 meters per frame...
@@ -178,12 +212,12 @@ public class Controller : MonoBehaviour
             oldZMovement = zMovement;
             oldXMovement = xMovement;
 
-            stillMoving = true;
+            //stillMoving = true;
         }
 
         else if ( zMovement == 0f && xMovement == 0f )
         {
-            stillMoving = false;
+            //stillMoving = false;
             oldZMovement = 0f;
             oldXMovement = 0f;
         }
