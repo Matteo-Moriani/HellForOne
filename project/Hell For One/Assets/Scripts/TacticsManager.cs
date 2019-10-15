d0c24b2f56c77a1aef031f92f33014b26f3981b1
andreacarrarini <andrea.carrarini.18@gmail.com>
2019-10-15 15:22:42 +0200

    setting pad buttons

--- project/Hell For One/Assets/Scripts/TacticsManager.cs
+++ project/Hell For One/Assets/Scripts/TacticsManager.cs
@@ -13,25 +13,49 @@ public class TacticsManager : MonoBehaviour
         GroupD
     }
 
+    [Header( "Input" )]
+    [SerializeField]
+    private bool cross, square, triangle, circle, L1, R1, L2, R2, R3;
+
     private GroupBehaviour.State[] tacticArray;
     private Group[] groupsArray;
 
     private int tacticsIndex, groupsIndex = 0;
 
-    void Start()
+    public void FillArrays()
     {
         tacticArray = new GroupBehaviour.State[ 4 ];
-        foreach(GroupBehaviour.State s in (GroupBehaviour.State[]) Enum.GetValues( typeof( GroupBehaviour.State ) ) )
+        foreach ( GroupBehaviour.State s in ( GroupBehaviour.State[] ) Enum.GetValues( typeof( GroupBehaviour.State ) ) )
         {
             tacticArray[ tacticsIndex ] = s;
-            tacticsIndex ++;
+            tacticsIndex++;
         }
 
-        groupsArray
+        groupsArray = new Group[ 4 ];
+        foreach ( Group g in ( Group[] ) Enum.GetValues( typeof( Group ) ) )
+        {
+            groupsArray[ groupsIndex ] = g;
+            groupsIndex++;
+        }
+    }
+
+    void Start()
+    {
+        FillArrays();
     }
 
     void Update()
     {
-        
+        cross = Input.GetButton( "cross" );
+        square = Input.GetButton( "square" );
+        triangle = Input.GetButtonDown( "triangle" );
+        circle = Input.GetButtonDown( "circle" );
+        L1 = Input.GetButtonDown( "L1" );
+        R1 = Input.GetButtonDown( "R1" );
+        L2 = Input.GetButtonDown( "L2" );
+        R2 = Input.GetButtonDown( "R2" );
+        R3 = Input.GetButtonDown( "R3" );
+
+
     }
 }
