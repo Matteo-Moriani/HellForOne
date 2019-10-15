e8ab02ff0e65cc0d83ba7a7a55cdaf044f89b43e
Matteo Moriani <moriamatte@gmail.com>
2019-10-15 18:57:56 +0200

    boss now with fsm and behavior tree

--- project/Hell For One/Assets/Scripts/AI/BossBehavior.cs
+++ project/Hell For One/Assets/Scripts/AI/BossBehavior.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-//[RequireComponent(typeof(NavMeshAgent))]
-
 public class BossBehavior : MonoBehaviour
 {
     public float speed = 8f;
@@ -12,6 +10,7 @@ public class BossBehavior : MonoBehaviour
     public float rotSpeed = 0.1f;
     public float stopDist = 4f;
     public float pause = 3f;
+    public float initialHP = 100f;
 
     private GameObject[] demonGroups;
     private GameObject currentTarget;
@@ -20,93 +19,122 @@ public class BossBehavior : MonoBehaviour
     private readonly float singleAttackProb = 0.6f;
     private readonly float groupAttackProb = 0.3f;
     private readonly float globalAttackProb = 0.1f;
-    // initialized at true to avoid attacking himself at the beginning
-    private bool attacked = true;
-    
+    private float crisis = 0f;
+    private float crisisMax = 50f;
+    private float hp;
+    private FSM bossFSM;
+    private bool timerRunning = false;
+    private bool resetFightingBT = false;
+    private CRBT.BehaviorTree FightingBT;
+    private Coroutine fightingCR;
+    private bool canWalk = false;
+
+
+    FSMState waitingState, fightingState, stunnedState;
+
+    [SerializeField]
+    private float fsmReactionTime = 1f;
+    private float btReactionTime = 0.05f;
+
+    public bool PlayerApproaching() {
+        //if(playerDistance < tot)
+        //    return true;
+        return true;
+    }
+
+    public bool CrisisFull() {
+        if(crisis >= crisisMax)
+            return true;
+        return false;
+    }
+
+    public bool LifeIsHalven() {
+        if(hp <= initialHP/2)
+            return true;
+        return false;
+    }
+
+    public bool RecoverFromStun() {
+        StartCoroutine(WaitSeconds(5));
+        return true;
+    }
+
+    // The coroutine that cycles through the FSM
+    public IEnumerator MoveThroughFSM() {
+        while(true) {
+            bossFSM.Update();
+            yield return new WaitForSeconds(fsmReactionTime);
+        }
+    }
+
     void Start()
     {
+        FSMTransition t0 = new FSMTransition(PlayerApproaching);
+        FSMTransition t1 = new FSMTransition(CrisisFull);
+        FSMTransition t2 = new FSMTransition(LifeIsHalven);
+        FSMTransition t3 = new FSMTransition(RecoverFromStun);
+
+        FightingBT = FightingBTBuilder();
+
+        waitingState = new FSMState();
+
+        fightingState = new FSMState();
+        fightingState.enterActions.Add(StartFightingCoroutine);
+        fightingState.exitActions.Add(StopFightingBT);
+
+        stunnedState = new FSMState();
+        
+
+        waitingState.AddTransition(t0, fightingState);
+        fightingState.AddTransition(t1, stunnedState);
+        fightingState.AddTransition(t2, stunnedState);
+        stunnedState.AddTransition(t3, fightingState);
+
+        bossFSM = new FSM(waitingState);
+
         // the initial target is himself to stay on his place for the first seconds
+        hp = initialHP;
         currentTarget = gameObject;
         demonGroups = GameObject.FindGameObjectsWithTag("Demon");
         aggroValues = new float[demonGroups.Length];
         probability = new float[demonGroups.Length + 1];
         probability[0] = 0f;
-        StartCoroutine(MainRoutine());
-        //foreach(GameObject g in demonGroups) {
-        //    Debug.Log(g.name + " initial aggro " + g.GetComponent<TargetScript>().GetAggro());
-        //}
+
+        StartCoroutine(MoveThroughFSM());
     }
     
     void Update()
     {
-        if(HorizDistFromTarget(currentTarget) <= 4f && attacked == false) {
-            RandomAttack(currentTarget);
-            attacked = true;
-        }
+       
     }
 
     void FixedUpdate() {
-        Vector3 targetPosition = currentTarget.transform.position;
 
-        Vector3 vectorToTarget = targetPosition - transform.position;
-        vectorToTarget.y = 0f;
-        Quaternion facingDir = Quaternion.LookRotation(vectorToTarget);
-        Quaternion newRotation = Quaternion.Slerp(transform.rotation, facingDir, rotSpeed);
-        transform.rotation = newRotation;
-
-
-        //in case for some reason I don't have a current target
+        // in case I don't have a target anymore for some reason
         if(currentTarget.transform) {
-            if(HorizDistFromTarget(currentTarget) > stopDist) {
+
+            // I'm always facing my last target
+            Vector3 targetPosition = currentTarget.transform.position;
+            Vector3 vectorToTarget = targetPosition - transform.position;
+            vectorToTarget.y = 0f;
+            Quaternion facingDir = Quaternion.LookRotation(vectorToTarget);
+            Quaternion newRotation = Quaternion.Slerp(transform.rotation, facingDir, rotSpeed);
+            transform.rotation = newRotation;
+
+            if(canWalk) {
                 transform.position += transform.forward * speed * Time.deltaTime;
             }
         }
     }
 
-    private void ChooseTarget() {
-        float totalAggro = 0f;
-        for(int i = 0; i < demonGroups.Length; i++) {
-            aggroValues[i] = demonGroups[i].GetComponent<TargetScript>().GetAggro();
-            totalAggro = totalAggro + demonGroups[i].GetComponent<TargetScript>().GetAggro();
-            probability[i + 1] = totalAggro;
-        }
-        //foreach(GameObject g in demonGroups) {
-        //    Debug.Log(g.name + " now has aggro " + g.GetComponent<TargetScript>().GetAggro());
-        //}
-
-        float random = Random.Range(0.001f, totalAggro);
-        //Debug.Log("random value: " + random);
-
-        for(int i = 1; i < probability.Length; i++) {
-            if(random > probability[i - 1] && random <= probability[i])
-                currentTarget = demonGroups[i - 1];
-        }
-
-        //Debug.Log("current target is " + currentTarget.name);
+    private IEnumerator WaitSeconds(int s) {
+        yield return new WaitForSeconds(s);
     }
 
-    private IEnumerator MainRoutine() {
-        while(true) {
-            yield return new WaitForSeconds(pause);
-
-            ChooseTarget();
-            attacked = false;
-            //MoveToTarget();
-        }
-    }
-
-    //private void MoveToTarget() {
-    //    GetComponent<NavMeshAgent>().destination = currentTarget.GetComponent<Transform>().position;
-    //}
-
-    private void RandomAttack(GameObject targetGroup) {
-        float random = Random.Range(0f, singleAttackProb+groupAttackProb+globalAttackProb);
-        if(random < singleAttackProb)
-            SingleAttack();
-        else if(random >= singleAttackProb && random < singleAttackProb + groupAttackProb)
-            GroupAttack();
-        else
-            GlobalAttack();
+    private IEnumerator Timer(int s) {
+        timerRunning = true;
+        yield return new WaitForSeconds(s);
+        timerRunning = false;
     }
 
     private void SingleAttack() {
@@ -124,5 +152,113 @@ public class BossBehavior : MonoBehaviour
     private float HorizDistFromTarget(GameObject target) {
         Vector3 targetPosition = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
         return (targetPosition - transform.position).magnitude;
+    }
+
+    public bool TimeoutStarted() {
+        return timerRunning;
+    }
+
+    public bool TimeoutEnded() {
+        return !timerRunning;
+    }
+
+    public bool ChooseTarget() {
+
+        if(GameObject.FindGameObjectsWithTag("Demon").Length == 0)
+            return false;
+
+        float totalAggro = 0f;
+        for(int i = 0; i < demonGroups.Length; i++) {
+            aggroValues[i] = demonGroups[i].GetComponent<TargetScript>().GetAggro();
+            totalAggro = totalAggro + demonGroups[i].GetComponent<TargetScript>().GetAggro();
+            probability[i + 1] = totalAggro;
+        }
+
+        float random = Random.Range(0.001f, totalAggro);
+
+        for(int i = 1; i < probability.Length; i++) {
+            if(random > probability[i - 1] && random <= probability[i])
+                currentTarget = demonGroups[i - 1];
+        }
+
+        return true;
+    }
+
+    public bool WaitABit() {
+        StartCoroutine(WaitSeconds(2));
+        return true;
+    }
+
+    public bool WalkToTarget() {
+        if(HorizDistFromTarget(currentTarget) > stopDist) {
+            canWalk = true;
+            return true;
+        } else {
+            canWalk = false;
+            return false;
+        }
+    }
+
+    public bool StartTimer() {
+        StartCoroutine(Timer(5));
+        return true;
+    }
+
+    public bool RandomAttack() {
+        float random = Random.Range(0f, singleAttackProb + groupAttackProb + globalAttackProb);
+        if(random < singleAttackProb)
+            SingleAttack();
+        else if(random >= singleAttackProb && random < singleAttackProb + groupAttackProb)
+            GroupAttack();
+        else
+            GlobalAttack();
+        
+        return true;
+    }
+
+    public CRBT.BehaviorTree FightingBTBuilder() {
+
+        CRBT.BTCondition c1 = new CRBT.BTCondition(TimeoutStarted);
+        CRBT.BTCondition c2 = new CRBT.BTCondition(TimeoutEnded);
+
+        CRBT.BTAction a1 = new CRBT.BTAction(ChooseTarget);
+        CRBT.BTAction a2 = new CRBT.BTAction(WaitABit);
+        CRBT.BTAction a3 = new CRBT.BTAction(StartTimer);
+        CRBT.BTAction a4 = new CRBT.BTAction(WalkToTarget);
+        CRBT.BTAction a5 = new CRBT.BTAction(RandomAttack);
+
+        CRBT.BTSelector sel1 = new CRBT.BTSelector(new CRBT.IBTTask[] { c1, a3 });
+
+        CRBT.BTSelector sel2 = new CRBT.BTSelector(new CRBT.IBTTask[] { c2, a4 });
+
+        CRBT.BTSequence seq1 = new CRBT.BTSequence(new CRBT.IBTTask[] { sel1, sel2 });
+
+        CRBT.BTDecoratorUntilFail uf1 = new CRBT.BTDecoratorUntilFail(seq1);
+
+        CRBT.BTSequence seq2 = new CRBT.BTSequence(new CRBT.IBTTask[] { a1, a2, uf1, a5 });
+
+        CRBT.BTDecoratorUntilFail utf2 = new CRBT.BTDecoratorUntilFail(seq2);
+
+        return new CRBT.BehaviorTree(utf2);
+    }
+
+    public IEnumerator FightingLauncherCR() {
+        while(FightingBT.Step())
+            yield return new WaitForSeconds(btReactionTime);
+    }
+
+    public void StartFightingCoroutine() {
+        if(resetFightingBT) {
+            FightingBT = FightingBTBuilder();
+            resetFightingBT = false;
+        }
+
+        fightingCR = StartCoroutine(FightingLauncherCR());
+    }
+
+    public void StopFightingBT() {
+        StopCoroutine(fightingCR);
+        fightingCR = null;
+        resetFightingBT = true;
     }
 }
