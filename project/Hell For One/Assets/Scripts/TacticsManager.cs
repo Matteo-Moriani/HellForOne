f47826a08e1aef31408316678f5ac734e4545dd9
andreacarrarini <andrea.carrarini.18@gmail.com>
2019-10-15 20:48:51 +0200

    still setting new input manager

--- project/Hell For One/Assets/Scripts/TacticsManager.cs
+++ project/Hell For One/Assets/Scripts/TacticsManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Input;
 
 public class TacticsManager : MonoBehaviour
 {
@@ -15,12 +16,14 @@ public class TacticsManager : MonoBehaviour
 
     [Header( "Input" )]
     [SerializeField]
-    private bool cross, square, triangle, circle, L1, R1, L2, R2, R3;
+    private bool cross, square, triangle, circle, L1, R1, L2, R2, R3 = false;
 
     private GroupBehaviour.State[] tacticArray;
     private Group[] groupsArray;
 
     private int tacticsIndex, groupsIndex = 0;
+
+    PS4Controller PS4Controller;
 
     public void FillArrays()
     {
@@ -39,6 +42,19 @@ public class TacticsManager : MonoBehaviour
         }
     }
 
+    public void ConfirmOrder()
+    {
+        cross = true;
+    }
+
+    void Awake()
+    {
+        PS4Controller = new PS4Controller();
+
+        // I don't care about the Context
+        PS4Controller.Gamplay.ConfirmOrder.performed += ctx => ConfirmOrder();
+    }
+
     void Start()
     {
         FillArrays();
@@ -46,15 +62,15 @@ public class TacticsManager : MonoBehaviour
 
     void Update()
     {
-        cross = Input.GetButton( "cross" );
-        square = Input.GetButton( "square" );
-        triangle = Input.GetButtonDown( "triangle" );
-        circle = Input.GetButtonDown( "circle" );
-        L1 = Input.GetButtonDown( "L1" );
-        R1 = Input.GetButtonDown( "R1" );
-        L2 = Input.GetButtonDown( "L2" );
-        R2 = Input.GetButtonDown( "R2" );
-        R3 = Input.GetButtonDown( "R3" );
+        //cross = Input.GetButton( "cross" );
+        //square = Input.GetButton( "square" );
+        //triangle = Input.GetButtonDown( "triangle" );
+        //circle = Input.GetButtonDown( "circle" );
+        //L1 = Input.GetButtonDown( "L1" );
+        //R1 = Input.GetButtonDown( "R1" );
+        //L2 = Input.GetButtonDown( "L2" );
+        //R2 = Input.GetButtonDown( "R2" );
+        //R3 = Input.GetButtonDown( "R3" );
 
 
     }
