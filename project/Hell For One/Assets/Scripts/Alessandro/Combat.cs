6535839ca1c61dcd67811f3a16a51b46dd449984
AlessandroPuddu <40497778+AlessandroPuddu@users.noreply.github.com>
2019-10-16 17:26:37 +0200

    Dash Fix - Basic Attack and block System

---
+++ project/Hell For One/Assets/Scripts/Alessandro/Combat.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject weaponCollider;
+
+    [SerializeField]
+    private float colliderDuration=0.1f;
+
+    private PlayerCombatCollisions playerCombatCollisions;
+
+    private bool isBlockCRoutineRunning = false;
+
+    private void Start()
+    {
+        playerCombatCollisions = weaponCollider.GetComponent<PlayerCombatCollisions>();    
+    }
+
+    void Update()
+    {
+        // left click
+        // -TODO- add controller button
+        if (Input.GetMouseButtonDown(0)) { 
+            Attack();
+        }
+        if (Input.GetMouseButton(1)) { 
+            if(!isBlockCRoutineRunning)
+                Block();       
+        }
+    }
+
+    void Attack() { 
+        StartCoroutine(AttackCoroutine());    
+    }
+
+    void Block() { 
+        StartCoroutine(BlockCoroutine());    
+    }
+
+    IEnumerator AttackCoroutine() { 
+        playerCombatCollisions.SetMode(PlayerCombatCollisions.Mode.Attack);
+        weaponCollider.SetActive(true);
+        yield return new WaitForSeconds(colliderDuration);
+        weaponCollider.SetActive(false);
+        playerCombatCollisions.SetMode(PlayerCombatCollisions.Mode.Idle);
+        yield return null;
+    }
+
+    IEnumerator BlockCoroutine() { 
+        isBlockCRoutineRunning = true;
+        weaponCollider.SetActive(true);
+        playerCombatCollisions.SetMode(PlayerCombatCollisions.Mode.Block);
+
+        while (Input.GetMouseButton(1)) { 
+            yield return null;
+        }
+
+        weaponCollider.SetActive(false);
+        playerCombatCollisions.SetMode(PlayerCombatCollisions.Mode.Idle);
+        isBlockCRoutineRunning = false;
+
+        yield return null;
+    }
+}
