61c9ef7f6a9ec233b91666fc555e41ebd8069663
Matteo Moriani <moriamatte@gmail.com>
2019-10-19 14:43:15 +0200

    demons move to target. mutual repulsion, merged demonBehavior script

---
+++ project/Hell For One/Assets/Scripts/AI/PushAway.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushAway : MonoBehaviour
+{
+    public float repulsion = 1f;
+    private GameObject[] demons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        demons = GameObject.FindGameObjectsWithTag("Demon");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        PushEnemies();
+    }
+
+    private float HorizDistFromTarget(Vector3 targetPosition) {
+        targetPosition = new Vector3(targetPosition.x, transform.position.y, targetPosition.z);
+        return (targetPosition - transform.position).magnitude - transform.localScale.x/2;
+    }
+
+    private void PushEnemies() {
+        foreach(GameObject demon in demons) {
+            if(HorizDistFromTarget(demon.GetComponent<Collider>().ClosestPoint(transform.position)) < 0.5f) {
+                demon.GetComponent<Rigidbody>().AddForce((demon.transform.position - transform.position), ForceMode.Impulse);
+            }
+        }
+    }
+}
--- project/Hell For One/Assets/Scripts/DemonBehaviour.cs
+++ project/Hell For One/Assets/Scripts/DemonBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DemonBehaviour : MonoBehaviour
 {
@@ -8,38 +9,72 @@ public class DemonBehaviour : MonoBehaviour
     private GameObject groupBelongingTo;
     private bool groupFound = false;
 
-    public void FindGroup()
-    {
-        GameObject[] go = GameObject.FindGameObjectsWithTag( "group" );
-        foreach ( GameObject g in go )
-        {
-            GameObject[] demonsArray = g.GetComponent<GroupBehaviour>().demons;
-            for ( int i = 0; i < demonsArray.Length; i++ )
-            {
-                if ( demonsArray[ i ] == null )
-                {
-                    demonsArray[ i ] = gameObject;
-                    groupFound = true;
-                    break;
-                }
-            }
-            if ( groupFound )
-            {
-                groupBelongingTo = g;
-                break;
-            }
-        }
-    }
+    public GameObject group;
+    public GameObject leader;
+    public float speed = 8f;
+
+    private GameObject currentTarget;
+    private Collider targetCollider;
+    private int groupSize;
+    private float repulsionDist = 0.7f;
+    private float meleeDist = 1.5f;
+    private float cohesionDist;
+    private Collider[] members = new Collider[17];
+    private bool canMove = true;
+
+    // repulsion su tutti i demonietti e sui boss
+    // cohesion sul suo gruppetto
+    // face nemico target o align demone del giocatore fuori dal combattimento
+
+    
 
     private void Start()
     {
         FindGroup();
+        currentTarget = group;
+        targetCollider = currentTarget.GetComponent<Collider>();
     }
 
     private void Update()
     {
         if ( !groupFound )
             FindGroup();
+    }
+
+    void FixedUpdate() {
+
+        if(HorizDistFromTargetBorders(currentTarget) < repulsionDist)
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * -1);
+        else if(HorizDistFromTargetBorders(currentTarget) > repulsionDist && HorizDistFromTargetBorders(currentTarget) < meleeDist)
+            GetComponent<NavMeshAgent>().destination = transform.position;
+        else
+            GetComponent<NavMeshAgent>().destination = targetCollider.ClosestPoint(transform.position);
+
+
+    }
+
+    private float HorizDistFromTargetBorders(GameObject target) {
+        Vector3 closestPoint = targetCollider.ClosestPoint(transform.position);
+        Vector3 targetPosition = new Vector3(closestPoint.x, transform.position.y, closestPoint.z);
+        return (targetPosition - transform.position).magnitude;
+    }
+
+    public void FindGroup() {
+        GameObject[] go = GameObject.FindGameObjectsWithTag("group");
+        foreach(GameObject g in go) {
+            GameObject[] demonsArray = g.GetComponent<GroupBehaviour>().demons;
+            for(int i = 0; i < demonsArray.Length; i++) {
+                if(demonsArray[i] == null) {
+                    demonsArray[i] = gameObject;
+                    groupFound = true;
+                    break;
+                }
+            }
+            if(groupFound) {
+                groupBelongingTo = g;
+                break;
+            }
+        }
     }
 
 }
