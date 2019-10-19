0e77b0f81de73969bdd30a46b10bd12063f0420a
Matteo Moriani <moriamatte@gmail.com>
2019-10-19 15:10:03 +0200

    ridivisi gli script per i demoni

--- project/Hell For One/Assets/Scripts/DemonBehaviour.cs
+++ project/Hell For One/Assets/Scripts/DemonBehaviour.cs
@@ -9,30 +9,14 @@ public class DemonBehaviour : MonoBehaviour
     private GameObject groupBelongingTo;
     private bool groupFound = false;
 
-    public GameObject group;
-    public GameObject leader;
-    public float speed = 8f;
-
-    private GameObject currentTarget;
-    private Collider targetCollider;
-    private int groupSize;
-    private float repulsionDist = 0.7f;
-    private float meleeDist = 1.5f;
-    private float cohesionDist;
-    private Collider[] members = new Collider[17];
-    private bool canMove = true;
-
-    // repulsion su tutti i demonietti e sui boss
-    // cohesion sul suo gruppetto
-    // face nemico target o align demone del giocatore fuori dal combattimento
+    
 
     
 
     private void Start()
     {
         FindGroup();
-        currentTarget = group;
-        targetCollider = currentTarget.GetComponent<Collider>();
+        
     }
 
     private void Update()
@@ -41,23 +25,9 @@ public class DemonBehaviour : MonoBehaviour
             FindGroup();
     }
 
-    void FixedUpdate() {
+    
 
-        if(HorizDistFromTargetBorders(currentTarget) < repulsionDist)
-            gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * -1);
-        else if(HorizDistFromTargetBorders(currentTarget) > repulsionDist && HorizDistFromTargetBorders(currentTarget) < meleeDist)
-            GetComponent<NavMeshAgent>().destination = transform.position;
-        else
-            GetComponent<NavMeshAgent>().destination = targetCollider.ClosestPoint(transform.position);
-
-
-    }
-
-    private float HorizDistFromTargetBorders(GameObject target) {
-        Vector3 closestPoint = targetCollider.ClosestPoint(transform.position);
-        Vector3 targetPosition = new Vector3(closestPoint.x, transform.position.y, closestPoint.z);
-        return (targetPosition - transform.position).magnitude;
-    }
+   
 
     public void FindGroup() {
         GameObject[] go = GameObject.FindGameObjectsWithTag("group");
