0895505e1db243b70176b075fdd8378eedd079ae
Federico Lippolis <federicolippolis94@gmail.com>
2019-10-21 21:14:36 +0200

    Ranged unit
    -Created a script for the pooler of lances
    -Created a script for the ranged unit
    -Created a prefab of the lance

---
+++ project/Hell For One/Assets/Scripts/Ranged units/Lancer.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lancer : MonoBehaviour
+{
+    [Tooltip("The target of the ranged unit.")]
+    public GameObject target;
+    [Tooltip("The spawn position of the lance.")]
+    public Vector3 lancePosition;
+
+    [Tooltip("The initial speed of the lance.")]
+    public float speed;
+    [Tooltip("If the lance follow a direct tragectory or not.")]
+    public bool direct;
+
+    public float angleCorrection;
+    [Tooltip("The maximum distance of attack of the ranged unit.")]
+    public float maxDistance;
+    [Tooltip("The mininum distance of attack of the ranged unit.")]
+    public float minDistance;
+    [Tooltip("The number of lances per second.")]
+    public float ratio;
+    
+
+    GameObject lance;
+    ObjectsPooler lances;
+    private float lastShot;
+    private float timespanShots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lances = GetComponent<ObjectsPooler>();
+        lastShot = Time.time;
+        timespanShots = 1f / ratio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Time.time - lastShot > timespanShots)
+        {
+            if (Launch(target))
+            {
+                lastShot = Time.time;
+            }
+        }   
+    }
+
+    public bool Launch(GameObject target)
+    {
+        float distance;
+        float alpha;
+        if (target == null)
+        {
+            Debug.LogError("Target cannot be null");
+        }
+
+        distance = Vector3.Distance(transform.position, target.transform.position);
+        if (distance < minDistance || distance > maxDistance)
+        {
+            return false;
+        }
+
+        alpha = 0;
+
+        if (!calculateAngle(transform.position + lancePosition, target.transform.position, out alpha))
+        {
+            return false;
+        }
+
+        transform.forward = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z) - transform.position;
+        lance = lances.GetNotActiveObject();
+        lance.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        lance.transform.localPosition = lancePosition;
+        lance.transform.forward = new Vector3(target.transform.position.x, lance.transform.position.y, target.transform.position.z) - lance.transform.position;
+        lance.transform.localRotation = Quaternion.Euler(90f - alpha, angleCorrection, 0);
+        lance.SetActive(true);
+        lance.GetComponent<Rigidbody>().AddForce(lance.transform.up * (speed), ForceMode.VelocityChange);
+
+        return true;
+    }
+
+    bool calculateAngle(Vector3 from, Vector3 to, out float angle)
+    {
+        float x, y, g, v;
+        float tempResult;
+
+        x = Vector3.Distance(new Vector3(from.x, 0, from.z), new Vector3(to.x, 0, to.z));
+
+        y = to.y - from.y;
+        g = -Physics.gravity.y;
+        v = speed;
+
+        tempResult = g * x * x + 2 * y * v * v;
+        tempResult *= g;
+        tempResult = v * v * v * v - tempResult;
+        if (tempResult < 0)
+        {
+            angle = 0;
+            return false;
+            
+        }
+        
+        tempResult = Mathf.Sqrt(tempResult);
+
+        if (direct)
+        {
+            tempResult = v * v - tempResult;
+        }
+        else
+        {
+            tempResult = v * v + tempResult;
+        }
+        
+        tempResult = Mathf.Atan2(tempResult, g * x);
+
+        angle = tempResult * Mathf.Rad2Deg;
+
+        Debug.Log("x: " + x.ToString() + " alpha:" + angle.ToString());
+        return true;
+    }
+}
---
+++ project/Hell For One/Assets/Scripts/Ranged units/ObjectsPooler.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsPooler : MonoBehaviour
+{
+
+    [Tooltip("The object to duplicate in the pooler")]
+    public GameObject originalObject;
+
+    [Tooltip("The amount of objects in the pooler. At the start, the pooler is fill with this number of objects.")]
+    [Range(1, 100)]
+    public int amountInPooler;
+
+    [Tooltip("The number of objects added to the pooler if there is no objects available.")]
+    [Range(1, 100)]
+    public int incrementAmount;
+
+    private List<GameObject> pooler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pooler = new List<GameObject>();
+
+        for (int i = 0; i < amountInPooler; i++)
+        {
+            addNewObject();
+        }
+    }
+
+    /// <summary>
+    /// Gets an available object from the pooler. If none object is available, the 'incrementAmount' number of new objects are created.
+    /// </summary>
+    /// <returns>An available object from the pooler.</returns>
+    public GameObject GetObject()
+    {
+        GameObject newObject;
+
+        foreach (GameObject obj in pooler)
+        {
+
+            if (!obj.activeSelf)
+            {
+                activeObject(obj, obj.transform.position, obj.transform.rotation);
+                return obj;
+            }
+        }
+
+        amountInPooler += incrementAmount;
+
+        while (pooler.Count < amountInPooler - 1)
+        {
+            addNewObject();
+        }
+
+
+        newObject = addNewObject();
+        newObject.SetActive(true);
+
+        return newObject;
+    }
+
+    /// <summary>
+    /// Gets an available object from the pooler at the given position. If none object is available, the 'incrementAmount' number of new objects are created.
+    /// </summary>
+    /// <param name="position">Position of the object in the world.</param>
+    /// <returns>An available object from the pooler.</returns>
+    public GameObject GetObject(Vector3 position)
+    {
+        GameObject newObject;
+
+        foreach (GameObject obj in pooler)
+        {
+
+            if (!obj.activeSelf)
+            {
+                activeObject(obj, position, obj.transform.rotation);
+                return obj;
+            }
+        }
+
+        amountInPooler += incrementAmount;
+
+
+        while (pooler.Count < amountInPooler - 1)
+        {
+            addNewObject();
+        }
+
+        newObject = addNewObject();
+        activeObject(newObject, position, newObject.transform.rotation);
+        newObject.SetActive(true);
+
+        return newObject;
+    }
+
+    /// <summary>
+    /// Gets an available object from the pooler at the given position with the given rotation. If none object is available, the 'incrementAmount' number of new objects are created.
+    /// </summary>
+    /// <param name="position">Position of the object in the world.</param>
+    /// <param name="rotation">Rotation of the object in the world.</param>
+    /// <returns>An available object from the pooler.</returns>
+    public GameObject GetObject(Vector3 position, Quaternion rotation)
+    {
+        GameObject newObject;
+
+        foreach (GameObject obj in pooler)
+        {
+
+            if (!obj.activeSelf)
+            {
+                activeObject(obj, position, rotation);
+                return obj;
+            }
+        }
+
+        amountInPooler += incrementAmount;
+
+
+        while (pooler.Count < amountInPooler - 1)
+        {
+            addNewObject();
+        }
+
+
+        newObject = addNewObject();
+        activeObject(newObject, position, rotation);
+        newObject.SetActive(true);
+
+        return newObject;
+    }
+
+    /// <summary>
+    /// Gets an available object from the pooler, but not active. If none object is available, the 'incrementAmount' number of new objects are created.
+    /// </summary>
+    /// <returns>An available object from the pooler not active.</returns>
+    public GameObject GetNotActiveObject()
+    {
+        GameObject newObject;
+
+        foreach (GameObject obj in pooler)
+        {
+
+            if (!obj.activeSelf)
+            {
+                return obj;
+            }
+        }
+
+        amountInPooler += incrementAmount;
+
+        while (pooler.Count < amountInPooler - 1)
+        {
+            addNewObject();
+        }
+
+
+        newObject = addNewObject();
+        newObject.SetActive(false);
+
+        return newObject;
+    }
+
+    public GameObject addNewObject()
+    {
+        GameObject newObject;
+
+        newObject = Instantiate(originalObject, transform);
+        newObject.SetActive(false);
+        pooler.Add(newObject);
+
+        return newObject;
+    }
+
+    public void activeObject(GameObject obj, Vector3 position, Quaternion quaternion)
+    {
+        obj.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        obj.transform.position = position;
+        obj.transform.rotation = quaternion;
+        obj.SetActive(true);
+    }
+}
