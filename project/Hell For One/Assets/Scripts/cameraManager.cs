b373a6c04ebd27e668e30758147cfcdf78ee3d19
andreacarrarini <andrea.carrarini.18@gmail.com>
2019-10-29 11:29:14 +0100

    lock-on improved, still not the right feeling

--- project/Hell For One/Assets/Scripts/cameraManager.cs
+++ project/Hell For One/Assets/Scripts/cameraManager.cs
@@ -11,17 +11,25 @@ public class CameraManager : MonoBehaviour
     private float turnSpeed = 4.0f;
     private Vector3 offset;
 
-    // For testing sake
+    [SerializeField]
+    private bool closedEnvironment = false;
+
     [SerializeField]
     private float distance;
 
     [SerializeField]
     private bool isLocked = false;
 
+    public GameObject[] enemies;
+    public GameObject boss;
+
+    // To avoid takin too much inputs
+    private bool rightAxisInUse = false;
+
     private void FindPlayer()
     {
         player = GameObject.FindGameObjectWithTag( "Player" );
-        offset = new Vector3( 0.0f, 10.0f, -10.0f );
+        offset = new Vector3( 0.0f, 5.0f, -10.0f );
         target = player;
     }
 
@@ -50,18 +58,26 @@ public class CameraManager : MonoBehaviour
 
     private void Update()
     {
+        if ( closedEnvironment )
+            turnSpeed = 0.0f;
+        else
+            turnSpeed = 4.0f;
+
         if ( !player )
             FindPlayer();
 
+        // Remove lock-on
         if ( Input.GetButtonDown( "R3" ) && isLocked )
         {
             isLocked = false;
             target = player;
         }
+
+        // Start lock-on
         else if ( Input.GetButtonDown( "R3" ) && !isLocked )
         {
-            GameObject[] enemies = GameObject.FindGameObjectsWithTag( "LittleEnemy" );
-            GameObject boss = GameObject.FindGameObjectWithTag( "Boss" );
+            enemies = GameObject.FindGameObjectsWithTag( "LittleEnemy" );
+            boss = GameObject.FindGameObjectWithTag( "Boss" );
 
             if ( boss == null && enemies != null )
             {
@@ -74,6 +90,54 @@ public class CameraManager : MonoBehaviour
                 target = boss;
             }
         }
+
+        // Change lock-on target
+        float input = Input.GetAxis( "Vertical2" );
+        if ( (!rightAxisInUse && Mathf.Abs( input ) > 0.4f) && isLocked )
+        {
+            rightAxisInUse = true;
+
+            //Do nothing
+            if ( !boss && target == boss ) ;
+
+            // Cycle through the enemies
+            else
+            {
+                //enemies = GameObject.FindGameObjectsWithTag( "LittleEnemy" );
+                //boss = GameObject.FindGameObjectWithTag( "Boss" );
+                float minLeftDistance = -1 * Mathf.Infinity;
+                float minRightDistance = Mathf.Infinity;
+                GameObject leftNearestDemon = null;
+                GameObject rightNearestDemon = null;
+
+                foreach ( GameObject demon in enemies )
+                {
+                    float demonXAxis = transform.InverseTransformPoint( demon.transform.position ).x;
+
+                    if ( demonXAxis > 0f && demonXAxis < minRightDistance )
+                    {
+                        minRightDistance = demonXAxis;
+                        rightNearestDemon = demon;
+                    }
+
+                    if ( demonXAxis < 0f && demonXAxis > minLeftDistance )
+                    {
+                        minLeftDistance = demonXAxis;
+                        leftNearestDemon = demon;
+                    }
+                }
+
+                // Don't know why it is inverted
+                if ( input < 0 && rightNearestDemon )
+                    target = rightNearestDemon;
+                else if ( input > 0 && leftNearestDemon )
+                    target = leftNearestDemon;
+            }
+        }
+
+        // TODO The problem might not be here, but maybe it chooses again the already targeted enemy when it should change, need to check
+        else if ( Mathf.Abs( input ) <= 0.2f )
+            rightAxisInUse = false;
     }
 
     void LateUpdate()
