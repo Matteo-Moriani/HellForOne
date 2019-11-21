707f9c26f834577eaeefc804aa5e0cf46d0dea8a
AlessandroPuddu <40497778+AlessandroPuddu@users.noreply.github.com>
2019-11-21 13:47:01 +0100

    Audio files added

---
+++ project/Hell For One/Assets/Scripts/Audio/AudioUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioUtilities : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+
+    public void PlayRandomClip() { 
+        AudioSource audioSource = GetComponent<AudioSource>();
+        
+        AudioClip clip = clips[Random.Range(0,clips.Length - 1)];
+
+        if(clip != null) {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+        else { 
+            Debug.Log(this.transform.root.name + " Tried to play a clip but didn't found it");    
+        }
+    }
+}
