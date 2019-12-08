95f2fa7e051798b4b57682ee23968be0ca93e40c
andreacarrarini <andrea.carrarini.18@gmail.com>
2019-12-08 13:30:24 +0100

    invisible walls work

---
+++ project/Hell For One/Assets/Scripts/Triggers/InvisibleWallsTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleWallsTrigger : MonoBehaviour
+{
+    public void OnTriggerEnter( Collider other )
+    {
+        if ( other.gameObject.layer == LayerMask.NameToLayer( "Player" ) )
+        {
+            Physics.IgnoreLayerCollision( LayerMask.NameToLayer( "Player" ), LayerMask.NameToLayer( "InvisibleWalls" ), false );
+            Physics.IgnoreLayerCollision( LayerMask.NameToLayer( "InvisibleWalls" ), LayerMask.NameToLayer( "Player" ), false );
+        }
+    }
+}
