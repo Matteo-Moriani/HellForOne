abacfd5db4bd41879831d90543e5d9882c174f3b
AlessandroPuddu <40497778+AlessandroPuddu@users.noreply.github.com>
2019-12-18 12:59:54 +0100

    Blink fixed and work for final boss too

--- project/Hell For One/Assets/Scripts/Combat System/PlayerHitFeedback.cs
+++ project/Hell For One/Assets/Scripts/Combat System/PlayerHitFeedback.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerHitFeedback : MonoBehaviour
 {
+    [SerializeField]
+    private Material[] finalBossMaterials;
+
+    private Stats.Type type;
+
     private Renderer renderer;
+
+    private Color startingColor;
 
     private CombatEventsManager combatEventsManager;
 
@@ -18,23 +25,20 @@ public class PlayerHitFeedback : MonoBehaviour
     private void OnEnable()
     {
         combatEventsManager.onBeenHit += OnBeenHIt;
+        combatEventsManager.onDeath += OnDeath;
     }
 
     private void OnDisable()
     {
         combatEventsManager.onBeenHit -= OnBeenHIt;
+        combatEventsManager.onDeath -= OnDeath;
     }
 
     private void Start()
     {
         renderer = this.GetComponent<Renderer>();
-    }
-
-    private void Update()
-    {
-       //float i = renderer.material.GetFloat("_Surfacetype");
-       //Debug.Log(i);
-       Debug.Log(renderer.material.IsKeywordEnabled("_SURFACE_TYPE_TRANSPARENT"));
+        startingColor = renderer.material.GetColor("_EmissiveColor");
+        type = this.transform.root.GetComponent<Stats>().type;
     }
 
     private void OnBeenHIt(Stats attackerStats)
@@ -47,15 +51,47 @@ public class PlayerHitFeedback : MonoBehaviour
     }
 
     private IEnumerator BlinkCoroutine(Stats attackerStats)
-    {
-        Color startingColor = renderer.material.GetColor("_EmissiveColor");
-      
-        renderer.material.SetColor("_EmissiveColor", Color.red);
-        
-        yield return new WaitForSeconds(0.1f);
+    { 
+        if(this.transform.root.gameObject.name == "Boss") {
+            foreach (Material material in finalBossMaterials)
+            {
+                material.SetColor("_EmissiveColor", Color.red);
+            }
 
-        renderer.material.SetColor("_EmissiveColor", startingColor);
+            yield return new WaitForSeconds(0.1f);
 
-        blinkCR = null;
+            foreach (Material material in finalBossMaterials)
+            {
+                // Black means no emission
+                material.SetColor("_EmissiveColor", Color.black);
+            }
+
+            blinkCR = null;
+        }
+        else {
+            renderer.material.SetColor("_EmissiveColor", Color.red);
+
+            yield return new WaitForSeconds(0.1f);
+
+            // Maybe we can use Color.black instead of this
+            renderer.material.SetColor("_EmissiveColor", startingColor);
+
+            blinkCR = null;
+        }
+    }
+
+    private void OnDeath() {
+        // Boss has strange material setup, so we need a different approach.
+        if (this.transform.root.gameObject.name == "Boss") {
+            foreach(Material material in finalBossMaterials) { 
+                material.SetColor("_EmissiveColor",Color.black);    
+            }
+        }
+        // For everyone else this should be ok
+        else
+        {
+            // Maybe we can use Color.black instead of this
+            renderer.material.SetColor("_EmissiveColor", startingColor);
+        }    
     }
 }
