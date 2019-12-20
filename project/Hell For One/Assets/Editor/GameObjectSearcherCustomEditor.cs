8fef8b4194199624912f0bea7e5540cd1660a716
andreacarrarini <andrea.carrarini.18@gmail.com>
2019-12-20 16:46:32 +0100

    camera almost fixed there is still a bug with lock on after reincarnation

---
+++ project/Hell For One/Assets/Editor/GameObjectSearcherCustomEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using UnityEditor;
+
+[CustomEditor( typeof( GameObjectSearcher ) )]
+[System.Serializable]
+public class GameObjectSearcherCustomEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var target_cs = ( GameObjectSearcher ) target;
+        DrawDefaultInspector();
+
+        GameObjectSearcher gameObjectSearcherScript = target_cs;
+
+        if ( !Application.isPlaying )
+        {
+            string tagStr = gameObjectSearcherScript.searchTag;
+
+            tagStr = EditorGUILayout.TagField( "Search Tag", tagStr );
+            if ( tagStr != gameObjectSearcherScript.searchTag )
+            {
+                gameObjectSearcherScript.searchTag = tagStr;
+            }
+        }
+    }
+}
