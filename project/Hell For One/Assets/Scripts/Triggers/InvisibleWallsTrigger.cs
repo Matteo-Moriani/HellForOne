415a5470190c0cc5a226de7b7a5a6d35c6f1d180
andreacarrarini <andrea.carrarini.18@gmail.com>
2020-01-03 18:04:39 +0100

    second camera fix

--- project/Hell For One/Assets/Scripts/Triggers/InvisibleWallsTrigger.cs
+++ project/Hell For One/Assets/Scripts/Triggers/InvisibleWallsTrigger.cs
@@ -4,16 +4,69 @@ using UnityEngine;
 
 public class InvisibleWallsTrigger : MonoBehaviour
 {
+
+    private GameObject flameCircle;
+    private GameObject firewallMidBoss, arenaWallMidBoss;
+    private GameObject firewallBoss, arenaWallBoss;
+
+    private void Start()
+    {
+        flameCircle = GameObject.FindGameObjectWithTag( "FlameCircle" );
+        firewallMidBoss = GameObject.Find( "firewallMidBoss" );
+        arenaWallMidBoss = GameObject.Find( "ArenaWallMidBoss" );
+        firewallBoss = GameObject.Find( "firewallBoss" );
+        arenaWallBoss = GameObject.Find( "ArenaWallBoss" );
+    }
+
+    private void DisableWallsBoss()
+    {
+        firewallBoss.SetActive( false );
+        arenaWallBoss.SetActive( false );
+    }
+
+    private void EnableWallBoss()
+    {
+        firewallBoss.SetActive( true );
+        arenaWallBoss.SetActive( true );
+    }
+
+    private void DisableArenaWallMidBoss()
+    {
+        arenaWallMidBoss.SetActive( false );
+    }
+
+    private void DisableFirewall2()
+    {
+        firewallMidBoss.SetActive( false );
+    }
+
+    private void EnableFlameCircle()
+    {
+        flameCircle.SetActive(true);
+    }
+
+    private void DisableFlameCircle()
+    {
+        flameCircle.SetActive( false );
+    }
+
     private void OnEnable()
     {
         BattleEventsManager.onBossBattleExit += OpenInvisibleWalls;
         BattleEventsManager.onBossBattleExit += DisableBossFlames;
+        BattleEventsManager.onBossBattleExit += DisableWallsBoss;
+        BattleEventsManager.onBossBattleExit += DisableFlameCircle;
         BattleEventsManager.onBattleExit += OpenInvisibleWalls;
+        BattleEventsManager.onBattleExit += DisableFirewall2;
+        BattleEventsManager.onBattleExit += DisableArenaWallMidBoss;
         BattleEventsManager.onBattleExit += DisableNormalFlames;
         BattleEventsManager.onBattleExit += LetDemonsPassFlames;
         BattleEventsManager.onBossBattleEnter += CloseInvisibleWalls;
+        BattleEventsManager.onBossBattleEnter += EnableFlameCircle;
         BattleEventsManager.onBossBattleEnter += EnableBossFlames;
+        BattleEventsManager.onBossBattleEnter += EnableWallBoss;
         BattleEventsManager.onBattleEnter += CloseInvisibleWalls;
+        BattleEventsManager.onBattleEnter += DisableFlameCircle;
         BattleEventsManager.onBattleEnter += EnableNormalFlames;
         DisableNormalFlames();
         DisableBossFlames();
