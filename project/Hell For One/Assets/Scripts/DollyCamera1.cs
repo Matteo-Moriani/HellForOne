107f3687eea463a4eeaed08707724b8ba802ff29
andreacarrarini <andrea.carrarini.18@gmail.com>
2020-02-05 19:01:12 +0100

    trailer

---
+++ project/Hell For One/Assets/Scripts/DollyCamera1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollyCamera1 : MonoBehaviour
+{
+    private Transform target;
+    private Cinemachine.CinemachineVirtualCamera cinemachineVirtualCamera;
+    private Cinemachine.CinemachineTrackedDolly dolly;
+    private GameObject midBoss;
+
+    void Start()
+    {
+        cinemachineVirtualCamera = GetComponent<Cinemachine.CinemachineVirtualCamera>();
+        dolly = cinemachineVirtualCamera.GetCinemachineComponent<Cinemachine.CinemachineTrackedDolly>();
+        target = GameObject.Find( "DollyTrackTarget" ).transform;
+        midBoss = GameObject.Find( "MidBoss" );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((target.position -transform.position).magnitude >= 30f )
+        {
+            dolly.m_AutoDolly.m_Enabled = true;
+        }
+
+        //if ( dolly.m_PathPosition >= 11 )
+        //{
+        //    cinemachineVirtualCamera.m_LookAt = midBoss.transform;
+        //}
+    }
+}
---
+++ project/Hell For One/Assets/Scripts/DollyTrackTargetMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollyTrackTargetMovement : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime; 
+    }
+}
