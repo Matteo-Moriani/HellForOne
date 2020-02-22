c1d20ccfa8d7e15c8774861724fa2db2942ab985
andreacarrarini <andrea.carrarini.18@gmail.com>
2020-02-22 14:56:20 +0100

    bosses clip

---
+++ project/Hell For One/Assets/Scripts/BossDollyCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDollyCamera : MonoBehaviour
+{
+    //private Transform target;
+    private Cinemachine.CinemachineVirtualCamera cinemachineVirtualCamera;
+    private Cinemachine.CinemachineTrackedDolly dolly;
+    private GameObject midBoss;
+    public float speed;
+
+    void Start()
+    {
+        cinemachineVirtualCamera = GetComponent<Cinemachine.CinemachineVirtualCamera>();
+        dolly = cinemachineVirtualCamera.GetCinemachineComponent<Cinemachine.CinemachineTrackedDolly>();
+        //target = GameObject.Find( "BossCameraTarget" ).transform;
+    }
+
+    void Update()
+    {
+        dolly.m_PathPosition += speed * Time.deltaTime;
+    }
+}
