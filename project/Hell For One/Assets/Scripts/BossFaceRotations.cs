7a0797704cdb21118db83ae41b5722c13d638ae2
andreacarrarini <andrea.carrarini.18@gmail.com>
2020-03-05 16:10:17 +0100

    BossFace prefab added to replace the aggro face on HUD

---
+++ project/Hell For One/Assets/Scripts/BossFaceRotations.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFaceRotations : MonoBehaviour
+{
+    private Vector3 cameraPosition, rotateDirection;
+    private bool bossFaceActive = true;
+    private Quaternion lookRotation;
+
+    void Update()
+    {
+        if ( bossFaceActive )
+        {
+            cameraPosition = Camera.main.transform.position;
+            rotateDirection = (cameraPosition - transform.position).normalized;
+            lookRotation = Quaternion.LookRotation( rotateDirection );
+            transform.rotation = Quaternion.Slerp( transform.rotation, lookRotation, 1 );
+
+        }
+    }
+}
