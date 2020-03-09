4b72a2ebaeb7a14e9ecd08b14984e17681d8692d
AlessandroPuddu <40497778+AlessandroPuddu@users.noreply.github.com>
2020-03-09 20:32:41 +0100

    Lava fix

--- project/Hell For One/Assets/3DArt/3DAsset/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
+++ project/Hell For One/Assets/3DArt/3DAsset/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
@@ -3,18 +3,64 @@ using System.Collections;
 
 public class ScrollingUVs_Layers : MonoBehaviour 
 {
-	//public int materialIndex = 0;
-	public Vector2 uvAnimationRate = new Vector2( 1.0f, 0.0f );
-	public string textureName = "_MainTex";
+	[SerializeField]
+	[Tooltip("The material to use to animate lava")]
+	private Material lavaAnimationMaterial;
+
+	[SerializeField]
+	[Tooltip("Speed of the animation, restart game to see changes")]
+	private float xAanimationRate = 0.25f;
+
+	private string textureName = "_MainTex";
 	
-	Vector2 uvOffset = Vector2.zero;
-	
+	private Vector2 defaultTextureOffset;
+
+	private Renderer[] renderers;
+
+	private Vector2 uvOffset = Vector2.zero;
+
+	private void Awake()
+	{
+		renderers = this.gameObject.GetComponentsInChildren<Renderer>();
+		if(lavaAnimationMaterial != null) {
+			defaultTextureOffset = lavaAnimationMaterial.GetTextureOffset(textureName);
+		}
+		else {
+			ShowMaterialNotAssignedError();
+		}
+	}
+
+	private void Start()
+	{
+		if(lavaAnimationMaterial != null) {
+			foreach (Renderer rend in renderers)
+			{
+				if (rend != null)
+				{
+					rend.material = lavaAnimationMaterial;
+				}
+			}
+		}
+		else {
+			ShowMaterialNotAssignedError();
+		}
+
+		renderers = null;
+	}
+
 	void LateUpdate() 
 	{
-		uvOffset += ( uvAnimationRate * Time.deltaTime );
-		if( GetComponent<Renderer>().enabled )
-		{
-			GetComponent<Renderer>().sharedMaterial.SetTextureOffset( textureName, uvOffset );
+		uvOffset += (new Vector2(xAanimationRate,0.0f) * Time.deltaTime);
+
+		if (lavaAnimationMaterial != null) { 
+			lavaAnimationMaterial.SetTextureOffset(textureName,uvOffset);
 		}
+		else { 
+			ShowMaterialNotAssignedError();	
+		}
+	}
+
+	private void ShowMaterialNotAssignedError() {
+		Debug.LogError(this.name + " " + this.gameObject.name + " lavaAnimationMaterial not assigned");
 	}
 }
