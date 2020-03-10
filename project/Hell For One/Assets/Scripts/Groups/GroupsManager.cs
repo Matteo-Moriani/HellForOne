c2cfdc33c10a9658f670835d73202d4d559c50cb
AlessandroPuddu <40497778+AlessandroPuddu@users.noreply.github.com>
2020-03-10 16:43:32 +0100

    Groups manager

---
+++ project/Hell For One/Assets/Scripts/Groups/GroupsManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupsManager : MonoBehaviour
+{
+    private GameObject[] groups;
+    private static GroupsManager instance;
+
+    public GameObject[] Groups { get => groups; private set => groups = value; }
+    public static GroupsManager Instance { get => instance; private set => instance = value; }
+
+    private void Awake()
+    {
+        if(instance != null && instance != this) { 
+            Destroy(this.gameObject);
+        }
+        else { 
+            instance = this;
+        }
+
+        Groups = GameObject.FindGameObjectsWithTag("Group");
+    }
+}
