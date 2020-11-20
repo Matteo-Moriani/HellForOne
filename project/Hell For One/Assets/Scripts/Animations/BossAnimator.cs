cd9fbb4292b3c74d2ee058df0ecebea621f6a082
Matteo Moriani <moriamatte@gmail.com>
2020-11-20 17:02:42 +0100

    stun and pain animations, animators little refactor

--- project/Hell For One/Assets/Scripts/Animations/BossAnimator.cs
+++ project/Hell For One/Assets/Scripts/Animations/BossAnimator.cs
@@ -1,137 +1,12 @@
-﻿using UnityEngine;
-
-public class BossAnimator : MonoBehaviour
+﻿public class BossAnimator : EnemyAnimator
 {
-    #region Fields
-
-    private Animator animator;
-    private BossBehavior myBehaviour;
-    private Stats stats;
-    private CombatEventsManager combatEventsManager;
-    private NormalCombat normalCombat;
-    private BossBehavior bossBehaviour;
-    private bool moving = false;
-
-    #endregion
-
-    #region Unity methods
-
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
-        combatEventsManager = gameObject.GetComponent<CombatEventsManager>();
-        myBehaviour = gameObject.GetComponent<BossBehavior>();
-        stats = GetComponent<Stats>();
-
-        normalCombat = GetComponentInChildren<NormalCombat>();
-        bossBehaviour = GetComponent<BossBehavior>();
-    }
-
-    private void OnEnable()
-    {
-        if(stats != null)
-        {
-            stats.onDeath += OnDeath;
-        }
-
-        if(normalCombat != null)
-        {
-            normalCombat.onStartAttack += OnStartAttack;
-        }
-
-        if(bossBehaviour != null)
-        {
-            bossBehaviour.onStartMoving += OnStartMoving;
-            bossBehaviour.onStopMoving += OnStopMoving;
-        }
-    }
-
-    private void OnDisable()
-    {
-        if(stats != null)
-        {
-            stats.onDeath -= OnDeath;
-        }
-
-        if(normalCombat != null)
-        {
-            normalCombat.onStartAttack -= OnStartAttack;
-        }
-
-        if(bossBehaviour != null)
-        {
-            bossBehaviour.onStartMoving -= OnStartMoving;
-            bossBehaviour.onStopMoving -= OnStopMoving;
-        }
-    }
-
-    private void Update()
-    {
-        if(moving)
-            PlayMoveAnimation();
-        else
-            SetAllBoolsToFalse();
-    }
-
-    #endregion
-
-    #region Methods
-
-    public void PlaySingleAttackAnimation()
-    {
-        SetAllBoolsToFalse();
-        animator.SetTrigger("singleAttack");
-    }
-
-    public void PlayGroupAttackAnimation()
-    {
-        SetAllBoolsToFalse();
-        animator.SetTrigger("groupAttack");
-    }
-
     public void PlayGlobalAttackAnimation()
     {
         SetAllBoolsToFalse();
         animator.SetTrigger("globalAttack");
     }
 
-    public void PlayDeathAnimation()
-    {
-        SetAllBoolsToFalse();
-        animator.SetTrigger("death");
-    }
-
-    public void PlayMoveAnimation()
-    {
-        SetAllBoolsToFalse();
-        animator.SetBool("isMoving", true);
-    }
-
-    public void SetAllBoolsToFalse()
-    {
-        animator.SetBool("isMoving", false);
-    }
-
-    #endregion
-
-    #region Event handlers
-
-    private void OnDeath(Stats sender)
-    {
-        PlayDeathAnimation();
-    }
-
-    private void OnStartMoving()
-    {
-        moving = true;
-    }
-
-    private void OnStopMoving()
-    {
-        moving = false;
-    }
-
-    private void OnStartAttack(NormalCombat sender, GenericAttack attack)
+    private protected override void OnStartAttack(NormalCombat sender, GenericAttack attack)
     {
         switch(attack.name)
         {
@@ -148,6 +23,4 @@ public class BossAnimator : MonoBehaviour
                 break;
         }        
     }
-
-    #endregion
 }
---
+++ project/Hell For One/Assets/Scripts/Animations/EnemyAnimator.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+
+public abstract class EnemyAnimator : MonoBehaviour
+{
+    #region Fields
+
+    private protected Animator animator;
+    private AbstractBoss myBehaviour;
+    private Stats stats;
+    private CombatEventsManager combatEventsManager;
+    private NormalCombat normalCombat;
+    private StunReceiver stunReceiver;
+    private bool moving = false;
+    private bool stunned = false;
+
+    #endregion
+
+    #region Unity methods
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        combatEventsManager = gameObject.GetComponent<CombatEventsManager>();
+        myBehaviour = gameObject.GetComponent<AbstractBoss>();
+        stats = GetComponent<Stats>();
+        stunReceiver = GetComponentInChildren<StunReceiver>();
+        normalCombat = GetComponentInChildren<NormalCombat>();
+    }
+
+    private void OnEnable()
+    {
+        // TODO - registrare OnPain all'evento appropriato e creare da qualche parte il metodo di fine Pain che verrà richiamato come animation event a fine animazione
+
+        if(stats != null)
+        {
+            stats.onDeath += OnDeath;
+        }
+
+        if(normalCombat != null)
+        {
+            normalCombat.onStartAttack += OnStartAttack;
+        }
+
+        if(myBehaviour != null)
+        {
+            myBehaviour.onStartMoving += OnStartMoving;
+            myBehaviour.onStopMoving += OnStopMoving;
+        }
+
+        if(stunReceiver != null)
+        {
+            stunReceiver.onStartStun += OnStartStun;
+            stunReceiver.onStopStun += OnStopStun;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(stats != null)
+        {
+            stats.onDeath -= OnDeath;
+        }
+
+        if(normalCombat != null)
+        {
+            normalCombat.onStartAttack -= OnStartAttack;
+        }
+
+        if(myBehaviour != null)
+        {
+            myBehaviour.onStartMoving -= OnStartMoving;
+            myBehaviour.onStopMoving -= OnStopMoving;
+        }
+
+        if(stunReceiver != null)
+        {
+            stunReceiver.onStartStun -= OnStartStun;
+            stunReceiver.onStopStun -= OnStopStun;
+        }
+    }
+
+    private void Update()
+    {
+        if(moving)
+            PlayMoveAnimation();
+        else if(stunned)
+            PlayStunAnimation();
+        else
+            SetAllBoolsToFalse();
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void PlaySingleAttackAnimation()
+    {
+        SetAllBoolsToFalse();
+        animator.SetTrigger("singleAttack");
+    }
+
+    public void PlayGroupAttackAnimation()
+    {
+        SetAllBoolsToFalse();
+        animator.SetTrigger("groupAttack");
+    }
+
+    public void PlayDeathAnimation()
+    {
+        SetAllBoolsToFalse();
+        animator.SetTrigger("death");
+    }
+
+    public void PlayPainAnimation()
+    {
+        SetAllBoolsToFalse();
+        animator.SetTrigger("pain");
+    }
+
+    public void PlayMoveAnimation()
+    {
+        SetAllBoolsToFalse();
+        animator.SetBool("isMoving", true);
+    }
+
+    public void PlayStunAnimation()
+    {
+        SetAllBoolsToFalse();
+        animator.SetBool("isStunned", true);
+    }
+
+    public void SetAllBoolsToFalse()
+    {
+        animator.SetBool("isMoving", false);
+        animator.SetBool("isStunned", false);
+    }
+
+    #endregion
+
+    #region Event handlers
+
+    private void OnDeath(Stats sender)
+    {
+        PlayDeathAnimation();
+    }
+
+    private void OnPain()
+    {
+        PlayPainAnimation();
+    }
+
+    private void OnStartMoving()
+    {
+        moving = true;
+    }
+
+    private void OnStopMoving()
+    {
+        moving = false;
+    }
+
+    private void OnStartStun()
+    {
+        stunned = true;
+    }
+
+    private void OnStopStun()
+    {
+        stunned = false;
+    }
+
+    private protected abstract void OnStartAttack(NormalCombat sender, GenericAttack attack);
+
+    #endregion
+}
