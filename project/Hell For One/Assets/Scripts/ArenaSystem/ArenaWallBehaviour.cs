ca7db85e8ce3265882dd931291af3b4c115468ca
Matteo Moriani <moriamatte@gmail.com>
2020-12-10 19:50:29 +0100

    arena elements refactored

---
+++ project/Hell For One/Assets/Scripts/ArenaSystem/ArenaWallBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using ArenaSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaWallBehaviour : MonoBehaviour, IArenaObserver
+{
+    private Collider _collider;
+    private ParticleSystem[] _particles;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider>();
+        _particles = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    public void EnterBattle(ArenaManager subject)
+    {
+        _collider.enabled = true;
+        foreach(ParticleSystem p in _particles)
+        {
+            p.Play();
+        }
+    }
+
+    public void ExitBattle(ArenaManager subject)
+    {
+        _collider.enabled = false;
+        foreach(ParticleSystem p in _particles)
+        {
+            p.Stop();
+        }
+    }
+
+    public void PrepareBattle(ArenaManager subject)
+    {
+        subject.NotifyBattlePrepared(this);
+    }
+}
---
+++ project/Hell For One/Assets/Scripts/ArenaSystem/SpectatorsBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using ArenaSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectatorsBehaviour : MonoBehaviour, IArenaObserver
+{
+    private SkinnedMeshRenderer[] _meshRenderers;
+    
+    private void Awake()
+    {
+        _meshRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
+    }
+
+    private void Start()
+    {
+        foreach(SkinnedMeshRenderer s in _meshRenderers)
+        {
+            s.enabled = false;
+        }
+    }
+
+    public void EnterBattle(ArenaManager subject)
+    {
+
+    }
+
+    public void ExitBattle(ArenaManager subject)
+    {
+        foreach(SkinnedMeshRenderer s in _meshRenderers)
+        {
+            s.enabled = false;
+        }
+    }
+
+    public void PrepareBattle(ArenaManager subject)
+    {
+        foreach(SkinnedMeshRenderer s in _meshRenderers)
+        {
+            s.enabled = true;
+        }
+        subject.NotifyBattlePrepared(this);
+    }
+}
