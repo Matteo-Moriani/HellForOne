77548abc73d67fe6601051fbda03e8bbe415db3f
AlessandroPuddu <alessandro.puddu94@gmail.com>
2020-12-19 17:47:41 +0100

    Imps and leader props

---
+++ project/Hell For One/Assets/Scripts/ReincarnationSystem/ImpPropBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ReincarnationSystem
+{
+    public class ImpPropBehavior : MonoBehaviour, IReincarnationObserver
+    {
+        private MeshRenderer[] _renderers;
+
+        private void Awake()
+        {
+            _renderers = GetComponentsInChildren<MeshRenderer>();
+        }
+
+        public void StartLeader()
+        {
+            foreach (MeshRenderer meshRenderer in _renderers)
+            {
+                meshRenderer.enabled = false;
+            }
+        }
+
+        public void StopLeader()
+        {
+        }    
+    }
+}
---
+++ project/Hell For One/Assets/Scripts/ReincarnationSystem/LeaderPropBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace ReincarnationSystem
+{
+    public class LeaderPropBehaviour : MonoBehaviour, IReincarnationObserver
+    {
+        private MeshRenderer[] _renderers;
+
+        private void Awake()
+        {
+            _renderers = GetComponentsInChildren<MeshRenderer>();
+        }
+
+        public void StartLeader()
+        {
+            foreach (MeshRenderer meshRenderer in _renderers)
+            {
+                meshRenderer.enabled = true;
+            }
+        }
+
+        public void StopLeader()
+        {
+        }
+    }
+}
