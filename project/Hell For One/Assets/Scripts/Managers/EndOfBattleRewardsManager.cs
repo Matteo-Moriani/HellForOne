5f19b0e1f94f59ee15badb781b3654184a9c8cb7
AlessandroPuddu <alessandro.puddu94@gmail.com>
2020-12-20 19:58:54 +0100

    Battle end reward

---
+++ project/Hell For One/Assets/Scripts/Managers/EndOfBattleRewardsManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using ArenaSystem;
+using HordeSystem;
+using ReincarnationSystem;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Managers
+{
+    public class EndOfBattleRewardsManager : MonoBehaviour
+    {
+        [SerializeField] private int battleEndImps;
+        [SerializeField] private float ray;
+        
+        private EndOfBattleRewardsManager _instance;
+
+        private void Awake()
+        {
+            if (_instance == null)
+                _instance = this;
+            else
+                Destroy(this);
+        }
+
+        private void OnEnable()
+        {
+            ArenaManager.OnGlobalEndBattle += OnGlobalEndBattle;
+        }
+
+        private void OnDisable()
+        {
+            ArenaManager.OnGlobalEndBattle += OnGlobalEndBattle;
+        }
+
+        private void OnGlobalEndBattle(ArenaManager obj)
+        {
+            StartCoroutine(SpawnCoroutine());
+        }
+
+        private IEnumerator SpawnCoroutine()
+        {
+            for (int i = 0; i < battleEndImps; i++)
+            {
+                Vector2 random = Random.insideUnitCircle * ray;
+                
+                HordeManager.Instance.SpawnImp(ReincarnationManager.Instance.CurrentLeader.transform.position + new Vector3(random.x,0f,random.y),Quaternion.identity);
+
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+    }
+}
