9f3d4e92b311684e9374231ffc2c6b4d32cfca8b
AlessandroPuddu <alessandro.puddu94@gmail.com>
2020-12-20 11:47:06 +0100

    Spear and shiled hide in recruit

--- project/Hell For One/Assets/Scripts/ReincarnationSystem/ImpPropBehavior.cs
+++ project/Hell For One/Assets/Scripts/ReincarnationSystem/ImpPropBehavior.cs
@@ -1,14 +1,46 @@
-﻿using UnityEngine;
+﻿using System;
+using TacticsSystem;
+using UnityEngine;
 
 namespace ReincarnationSystem
 {
     public class ImpPropBehavior : MonoBehaviour, IReincarnationObserver
     {
         private MeshRenderer[] _renderers;
-
+        private ImpRecruitBehaviour _impRecruitBehaviour;
+        
         private void Awake()
         {
             _renderers = GetComponentsInChildren<MeshRenderer>();
+            _impRecruitBehaviour = transform.root.GetComponent<ImpRecruitBehaviour>();
+        }
+
+        private void OnEnable()
+        {
+            _impRecruitBehaviour.OnStartRecruit += OnStartRecruit;
+            _impRecruitBehaviour.OnStopRecruit += OnStopRecruit;
+        }
+        
+        private void OnDisable()
+        {
+            _impRecruitBehaviour.OnStartRecruit -= OnStartRecruit;
+            _impRecruitBehaviour.OnStopRecruit -= OnStopRecruit;
+        }
+
+        private void OnStartRecruit()
+        {
+            foreach (MeshRenderer meshRenderer in _renderers)
+            {
+                meshRenderer.enabled = false;
+            }
+        }
+
+        private void OnStopRecruit()
+        {
+            foreach (MeshRenderer meshRenderer in _renderers)
+            {
+                meshRenderer.enabled = true;
+            }
         }
 
         public void StartLeader()
