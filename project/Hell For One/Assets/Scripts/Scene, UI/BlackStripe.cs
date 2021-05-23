ef8516f447ca752e6fbd83574232c81350881f0d
Matteo Moriani <moriamatte@gmail.com>
2021-05-23 10:40:22 +0200

    hud non sparisce durante cutscene (serve al tutorial)

--- project/Hell For One/Assets/Scripts/Scene, UI/BlackStripe.cs
+++ project/Hell For One/Assets/Scripts/Scene, UI/BlackStripe.cs
@@ -21,13 +21,13 @@ public class BlackStripe : MonoBehaviour
 
     private void OnGlobalSetupBattle(ArenaManager arenaManager) {
         GetComponent<Image>().enabled = true;
-        GameObject.FindGameObjectWithTag("HUD").GetComponent<Canvas>().enabled = false;
+        //GameObject.FindGameObjectWithTag("HUD").GetComponent<Canvas>().enabled = false;
         //GameObject.FindGameObjectWithTag( "UIOrders" ).SetActive( false );
     }
 
     private void OnGlobalStartBattle(ArenaManager arenaManager) {
         GetComponent<Image>().enabled = false;
-        GameObject.FindGameObjectWithTag("HUD").GetComponent<Canvas>().enabled = true;
+        //GameObject.FindGameObjectWithTag("HUD").GetComponent<Canvas>().enabled = true;
         //GameObject.FindGameObjectWithTag( "UIOrders" ).SetActive( true );
     }
 }
